b152ce3932d0d465b43f9826607e82ac96bcaa70
Danya Pavlov <fRo0neeee@gmail.com>
2022-06-13 21:45:04 +0300

    экшн на профиль

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,32 @@
-document.querySelector(".profile__edit-button").addEventListener('click', function(){
-  document.querySelector(".popup").classList.add("popup_opened");
+//modal action
+let editButton = document.querySelector(".profile__edit-button");
+let closeButton = document.querySelector(".popup__close");
+let popup = document.querySelector(".popup");
+let submitButton = popup.querySelector(".popup__button");
+
+//main content
+let mainContent = document.querySelector(".content");
+let profileName = mainContent.querySelector(".profile__title");
+let profileAbout = mainContent.querySelector(".profile__quote");
+
+editButton.addEventListener('click', function(){
+  let popupValue = popup.querySelectorAll(".popup__item");
+  popupValue[0].value = profileName.textContent;
+  popupValue[1].value = profileAbout.textContent;
+  popup.classList.toggle("popup_opened");
 });
 
-document.querySelector(".popup__close").addEventListener('click', function(){
-  document.querySelector(".popup").classList.remove("popup_opened");
+closeButton.addEventListener('click', function(){
+  popup.classList.toggle("popup_opened");
 });
+
+submitButton.addEventListener('click', profileSave);
+
+
+function profileSave(evt) {
+  evt.preventDefault();
+  let popupValue = popup.querySelectorAll(".popup__item");
+  profileName.textContent = popupValue[0].value;
+  profileAbout.textContent = popupValue[1].value;
+  popup.classList.toggle("popup_opened");
+}
