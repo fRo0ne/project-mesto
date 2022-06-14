878e6ad88862a18ad0bbd4468c4e3af8976943e1
Danya Pavlov <fRo0neeee@gmail.com>
2022-06-14 18:25:42 +0300

    исправления после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -27,7 +27,8 @@ const initialCards = [
   ];
 
 //modal
-const popup = document.querySelector(".popup_edit-profile");
+const popups = document.querySelectorAll('.popup');
+const popupProfile = document.querySelector(".popup_edit-profile");
 const popupCard = document.querySelector(".popup_add-card");
 const popupImage = document.querySelector(".popup_image");
 const picturePopup = popupImage.querySelector(".popup__picture");
@@ -45,6 +46,7 @@ const urlCardInput = cardPopupElement.querySelector('[name="url"]');
 
 //main content
 const mainContent = document.querySelector(".content");
+const profileContainer = mainContent.querySelector(".profile");
 const profileName = mainContent.querySelector(".profile__title");
 const profileAbout = mainContent.querySelector(".profile__quote");
 const elements = mainContent.querySelector(".elements__container");
@@ -52,28 +54,52 @@ const elements = mainContent.querySelector(".elements__container");
 //template
 const cardTemplate = document.querySelector('#cards-element').content;
 
+// закрываем попап
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+// открываем попап
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+// создание карточек
+function createCard(link,name) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__image').alt = name;
+  cardElement.querySelector('.card__info').textContent = name;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', function (evt) {
+    deleteCard(evt.target.parentNode);
+  });
+  cardElement.querySelector('.card__like').addEventListener('click', function (evt) {
+    evt.target.classList.toggle("card__like_active");
+  });
+  cardElement.querySelector('.card__image').addEventListener('click', function (evt) {
+    openedCardPopup(evt.target);
+  });
+  return cardElement;
+}
+
 // сохранение информации в профиле
 function saveProfile(evt) {
   evt.preventDefault();
   profileName.textContent = nameSaveInput.value;
   profileAbout.textContent = aboutSaveInput.value;
-  popup.classList.toggle("popup_opened");
+  closePopup(popupProfile);
 }
+
 
 // добавление карточки на страницу
 function saveCard(evt) {
   evt.preventDefault();
   const nameMesto = textCardInput.value;
   const linkMesto = urlCardInput.value;
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-  cardElement.querySelector('.card__image').src = linkMesto;
-  cardElement.querySelector('.card__image').alt = nameMesto;
-  cardElement.querySelector('.card__info').textContent = nameMesto;
-  elements.prepend(cardElement);
-  popupCard.classList.toggle("popup_opened");
+  elements.prepend(createCard(linkMesto,nameMesto));
+  closePopup(popupCard);
   textCardInput.value = '';
   urlCardInput.value = '';
-
 }
 
 // удаление карточки со страницы
@@ -86,17 +112,13 @@ function openedCardPopup(cardElement) {
   picturePopup.src = cardElement.src;
   picturePopup.alt = cardElement.alt;
   figurePopup.textContent = cardElement.alt;
-  popupImage.classList.toggle("popup_opened");
+  openPopup(popupImage);
 }
 
 // добавляем контент на страницу
 function loadCards (initialCards) {
   initialCards.forEach((card) => {
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-  cardElement.querySelector('.card__image').src = card.link;
-  cardElement.querySelector('.card__image').alt = card.name;
-  cardElement.querySelector('.card__info').textContent = card.name;
-  elements.append(cardElement);
+  elements.append(createCard(card.link,card.name));
   });
 }
 
@@ -104,60 +126,40 @@ function loadCards (initialCards) {
 function openedPopupProfile() {
   nameSaveInput.value = profileName.textContent;
   aboutSaveInput.value = profileAbout.textContent;
-  popup.classList.toggle("popup_opened");
-}
-
-// закрываем попап, без привязки где был вызван
-function closePopup(popupElement) {
-  const popupParent = popupElement.parentNode;
-  if (popupParent.classList.contains('figure')) {
-    return popupImage.classList.toggle("popup_opened");
-  }
-  if (popupParent.classList.contains('popup__container') && popupParent.parentNode.classList.contains('popup_edit-profile')) {
-    return popup.classList.toggle("popup_opened");
-  }
-  textCardInput.value = '';
-  urlCardInput.value = '';
-  return popupCard.classList.toggle("popup_opened");
+  openPopup(popupProfile);
 }
 
 // открыаем попап для добавления карточек на страницу
 function openedCardActionPopup() {
-  popupCard.classList.toggle("popup_opened");
+  openPopup(popupCard);
 }
+
+//эвент на попап
+popups.forEach((popup) => {
+  popup.addEventListener('click', (evt) => {
+     if (evt.target.classList.contains('popup__close')) {
+        closePopup(popup);
+      }
+  });
+});
+
+// эвент форм, сохранение профиля и добавление карточек
+savePopupElement.addEventListener('submit', saveProfile);
+cardPopupElement.addEventListener('submit', saveCard);
 
 // загружаем карточки, после построения дом дерева
 document.addEventListener("DOMContentLoaded", loadCards(initialCards));
 
-// delegation
-document.addEventListener('click', function(event) {
-  const element = event.target;
-  // like action
-  if(element.classList.contains('card__like')) {
-    element.classList.toggle("card__like_active");
-  }
+// delegation profile
+profileContainer.addEventListener('click', function(evt) {
+  const element = evt.target;
   // edit action
-  else if (element.classList.contains('profile__edit-button')) {
+  if (element.classList.contains('profile__edit-button')) {
     openedPopupProfile();
-  }
-  // close popup action
-  else if (element.classList.contains('popup__close')) {
-    closePopup(element);
   }
   // add card action
   else if (element.classList.contains('profile__add-button')) {
     openedCardActionPopup();
   }
-  // delete card action
-  else if (element.classList.contains('card__delete-button')) {
-    deleteCard(element.parentNode);
-  }
-  // cards view-image action
-  else if (element.classList.contains('card__image')) {
-    openedCardPopup(element);
-  }
-
 });
 
-savePopupElement.addEventListener('submit', saveProfile);
-cardPopupElement.addEventListener('submit', saveCard);
