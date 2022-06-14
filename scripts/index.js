ec7b12b191985e28d75cea326f3a4d7e782cc793
Danya Pavlov <fRo0neeee@gmail.com>
2022-06-14 14:31:27 +0300

    клонирование объектов и добавление их на страницу, убрал формирование верстки попапа из жс

--- scripts/index.js
+++ scripts/index.js
@@ -27,8 +27,9 @@ const initialCards = [
   ];
 
 //modal
-const popup = document.querySelector(".popup");
-const popupImage = document.querySelector(".view-image");
+const popup = document.querySelector(".popup_edit-profile");
+const popupCard = document.querySelector(".popup_add-card");
+const popupImage = document.querySelector(".popup_image");
 
 //main content
 const mainContent = document.querySelector(".content");
@@ -36,30 +37,28 @@ const profileName = mainContent.querySelector(".profile__title");
 const profileAbout = mainContent.querySelector(".profile__quote");
 const elements = mainContent.querySelector(".elements__container");
 
+//template
+const cardTemplate = document.querySelector('#cards-element').content;
+
 // сохранение информации в профиле
 function saveProfile() {
-  let popupValue = popup.querySelectorAll(".popup__item");
+  const popupValue = popup.querySelectorAll(".popup__item");
   profileName.textContent = popupValue[0].value;
   profileAbout.textContent = popupValue[1].value;
   popup.classList.toggle("popup_opened");
-  popupValue[0].value = '';
-  popupValue[1].value = '';
 }
 
 // добавление карточки на страницу
 function saveCard() {
-  const popupValue = popup.querySelectorAll(".popup__item");
+  const popupValue = popupCard.querySelectorAll(".popup__item");
   const nameMesto = popupValue[0].value;
   const linkMesto = popupValue[1].value;
-  elements.insertAdjacentHTML('afterbegin',`<article class="card">
-  <button class="card__delete-button" type="button"></button>
-  <img class="card__image" src="${linkMesto}" alt="${nameMesto}">
-  <div class="card__about">
-    <h2 class="card__info">${nameMesto}</h2>
-    <button class="card__like" type="button"></button>
-  </div>
-  </article>`);
-  popup.classList.toggle("popup_opened");
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = linkMesto;
+  cardElement.querySelector('.card__image').alt = nameMesto;
+  cardElement.querySelector('.card__info').textContent = nameMesto;
+  elements.prepend(cardElement);
+  popupCard.classList.toggle("popup_opened");
   popupValue[0].value = '';
   popupValue[1].value = '';
 
@@ -72,59 +71,50 @@ function deleteCard(cardElement) {
 
 // открываем просмотр фото
 function openedCardPopup(cardElement) {
-  popupImage.innerHTML = `<figure class="figure">
-  <button class="popup__close" type="button"></button>
-  <img class="view-image__picture" src="${cardElement.src}" alt="${cardElement.alt}">
-  <figcaption class="figure__quote figure__quote_view-image">${cardElement.alt}</figcaption>
-</figure>`;
+  const picturePopup = popupImage.querySelector(".popup__picture");
+  const figurePopup = popupImage.querySelector(".figure__quote");
+  picturePopup.src = cardElement.src;
+  picturePopup.alt = cardElement.alt;
+  figurePopup.textContent = cardElement.alt;
   popupImage.classList.toggle("popup_opened");
 }
 
 // добавляем контент на страницу
 function loadCards (initialCards) {
   initialCards.forEach((card) => {
-    elements.insertAdjacentHTML('beforeend',`<article class="card">
-    <button class="card__delete-button" type="button"></button>
-    <img class="card__image" src="${card.link}" alt="${card.name}">
-    <div class="card__about">
-      <h2 class="card__info">${card.name}</h2>
-      <button class="card__like" type="button"></button>
-    </div>
-    </article>`);
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__image').alt = card.name;
+  cardElement.querySelector('.card__info').textContent = card.name;
+  elements.append(cardElement);
   });
 }
 
 // открываем попап для редактирования профиля
 function openedPopupProfile() {
-  let popupValue = popup.querySelectorAll(".popup__item");
+  const popupValue = popup.querySelectorAll(".popup__item");
   popupValue[0].value = profileName.textContent;
   popupValue[1].value = profileAbout.textContent;
   popup.classList.toggle("popup_opened");
 }
 
 // закрываем попап, без привязки где был вызван
-function closePopup() {
-  let popupValue = popup.querySelectorAll(".popup__item");
-  if (!popup.querySelector(".popup__button")) {
-    popup.querySelector(".popup__button-cards").classList.replace("popup__button-cards", "popup__button");
+function closePopup(popupElement) {
+  const popupParent = popupElement.parentNode;
+  if(popupParent.classList.contains('figure')) {
+    return popupImage.classList.toggle("popup_opened");
   }
-  popup.classList.toggle("popup_opened");
-  popupValue[0].placeholder = 'Имя';
-  popupValue[1].placeholder = 'Описание';
-  popupValue[0].value = '';
-  popupValue[1].value = '';
+  if(popupParent.classList.contains('popup__container')) {
+    const popupValue = popupParent.parentNode.querySelectorAll(".popup__item");
+    popupValue[0].value = '';
+    popupValue[1].value = '';
+    return popupParent.parentNode.classList.contains('popup_edit-profile') ? popup.classList.toggle("popup_opened") : popupCard.classList.toggle("popup_opened");
+  }
 }
 
 // открыаем попап для добавления карточек на страницу
 function openedCardActionPopup() {
-  let popupValue = popup.querySelectorAll(".popup__item");
-  let popupTitle = popup.querySelector(".popup__title");
-  let popupButton = popup.querySelector(".popup__button");
-  popupTitle.textContent = 'Новое место';
-  popupValue[0].placeholder = 'Название';
-  popupValue[1].placeholder = 'Ссылка на картинку';
-  popupButton.classList.replace("popup__button", "popup__button-cards");
-  popup.classList.toggle("popup_opened");
+  popupCard.classList.toggle("popup_opened");
 }
 
 // загружаем карточки, после построения дом дерева
@@ -143,10 +133,7 @@ document.addEventListener('click', function(event) {
   }
   // close popup action
   else if (element.classList.contains('popup__close')) {
-    if(element.parentNode.classList.contains('figure')) {
-      return popupImage.classList.toggle("popup_opened");
-    }
-    closePopup();
+    closePopup(element);
   }
   // save profile info action
   else if (element.classList.contains('popup__button')) {
@@ -161,7 +148,6 @@ document.addEventListener('click', function(event) {
   else if (element.classList.contains('popup__button-cards')) {
     event.preventDefault();
     saveCard();
-    element.classList.replace("popup__button-cards", "popup__button");
   }
   // delete card action
   else if (element.classList.contains('card__delete-button')) {
